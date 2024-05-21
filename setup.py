47b6a22e74a1d1a5b3fc7b04583aeb348a091b16
Ghaby-X <gabrielanyaele21@gmail.com>
2024-05-21 09:32:28 +0000

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup # type: ignore
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Prediction of activators of DAF-12 as potential anti-helminthic drug candidates',
+    author='Gabriel Anyaele',
+    license='',
+)
