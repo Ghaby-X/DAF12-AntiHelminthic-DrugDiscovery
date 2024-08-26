bd8a5e930aa6dbbe9ea4ec9920e704d74d996264
Ghaby-X <gabrielanyaele21@gmail.com>
2024-08-26 06:49:02 +0000

    robust testing

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -2,6 +2,22 @@ from sklearn.metrics import classification_report
 import matplotlib.pyplot as plt
 import numpy as np
 
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
+import seaborn as sns
+
+def score(y_test, y_pred):
+    return {
+        'accuracy_score': accuracy_score(y_test, y_pred),
+        'precision_score': precision_score(y_test, y_pred),
+        'recall_score': recall_score(y_test, y_pred),
+        'f1_score': f1_score(y_test, y_pred)
+    }
+
+
+
 def plot_classification_report(y_test, y_pred):
     report = classification_report(y_test, y_pred, output_dict=True)
     keys = ['0', '1']
@@ -20,3 +36,26 @@ def plot_classification_report(y_test, y_pred):
     plt.ylabel('Classes')
     plt.title('Classification Report with Support')
     plt.show()
+
+
+def plot_scores(y_test, y_pred):
+    plots = sns.barplot(x=score(y_test, y_pred).keys(), y=score(y_test, y_pred).values(), width=0.5)
+
+    # Iterating over the bars one-by-one
+    for bar in plots.patches:
+    
+    # Using Matplotlib's annotate function and
+    # passing the coordinates where the annotation shall be done
+    # x-coordinate: bar.get_x() + bar.get_width() / 2
+    # y-coordinate: bar.get_height()
+    # free space to be left to make graph pleasing: (0, 8)
+    # ha and va stand for the horizontal and vertical alignment
+        plots.annotate(format(bar.get_height(), '.2f'), 
+                    (bar.get_x() + bar.get_width() / 2, 
+                        bar.get_height()), ha='center', va='center',
+                    size=10, xytext=(0, 5),
+                    textcoords='offset points')
+
+    plt.title('Metrics score')
+    plt.ylabel('scores')
+    plt.xlabel('metrics')
